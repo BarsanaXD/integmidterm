82d81e3fb0c5a9516d2a79abd7248c5aa0b0fcae
barsanaxd <barsanaxd@gmail.com>
2022-10-22 19:34:41 +0800

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav>
+      <v-toolbar app >
+       <v-app-bar-nav-icon @click='toggleDrawer'></v-app-bar-nav-icon>
+      <v-toolbar-title >
+        <span class="font-weight-light">Barsana </span>
+        <span>Midterm Project</span>
+      </v-toolbar-title>
+      <v-spacer ></v-spacer>
+
+      <!-- dropdown menu -->
+    <v-menu
+      open-on-hover
+      
+    >
+      
+
+        <v-list >
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            router
+            :to="item.path" :prepend-icon="item.icon">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-menu>
+
+    
+      
+      
+      </v-toolbar>
+
+     <v-navigation-drawer id="aydol" app v-model="drawer" >
+       <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          router 
+          :to="item.path" :prepend-icon="item.icon">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          
+        </v-list-item>
+      
+     </v-navigation-drawer>
+  </nav>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+    
+  const drawer = ref(false)
+
+  const  items =  ref([
+           { title: 'Personal Life', icon: 'mdi-account', path: '/' },
+           { title: 'Skills', icon: 'mdi-view-dashboard', path: '/skills' },
+           { title: 'Basic Math Demo', icon: 'mdi-plus-one', path: '/basicMath' },
+           { title: 'String App', icon: 'mdi-code-string', path: '/stringApp'},
+           { title: 'Axios Demo', icon: 'mdi-api', path: '/axiosDemo'},
+           { title: 'Area Calculator', icon: 'mdi-calculator-variant-outline', path: '/area'},
+           { title: 'Quiz App', icon: 'mdi-lightbulb-on', path: '/quiz'},
+           
+           
+
+           
+        ])
+
+ function toggleDrawer(){
+  return drawer.value = !drawer.value
+ }
+
+ 
+</script>
+<style>
+ #aydol{
+    color: rgb(0, 0, 0);
+    background-color: rgba(247, 121, 121, 0.885);
+   }
+</style>
---
+++ src/views/AxiosDemo.vue
@@ -0,0 +1,107 @@
+<template>
+    <h1>Axios Demo Getting Free-to-Play Games Database FROM RAPID API</h1>
+
+    <div class="col">
+	  <div class="card" style="border-radius:50px;">
+		  <div class="card-header" style="border-radius:50px; background-color: rgb(82, 190, 252);" >
+			  <h1 style="text-align: center; padding: 10px;">{{covidData.title}}</h1>
+			  
+		  </div>
+		
+		</div>
+    <div class="container">
+  <div class="content">
+    <h1>What is {{covidData.title}}</h1>
+    <h3>{{covidData.description}}</h3>
+  </div>
+  <div class="content">
+    <h1>Genre</h1>
+    <h3>{{covidData.genre}}</h3>
+  </div>
+</div>
+	</div>
+
+
+    <v-img fill-height
+		  src={{covidData.thumbnail}}
+		  class="mb-5">
+  
+		</v-img>
+    
+      
+    
+</template>
+
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios'
+
+ 
+  const  covidData=ref([])
+
+   
+  async function getCovidData () {
+       axios({
+                method:'get',
+                url:'https://free-to-play-games-database.p.rapidapi.com/api/game', 
+                params: {id: '452'},
+                headers: {
+                    'X-RapidAPI-Key': '6337a3b3b6mshdf42295427ad5d6p15a551jsn6a13088ebfa4',
+                    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+                        }  
+            }).then( response => {
+       covidData.value=(response.data)
+       console.log(covidData.value)
+      })
+  
+  }
+
+
+  
+   onMounted( async () => {
+       await getCovidData()
+   })
+
+  
+</script>
+
+
+<style lang="scss">
+.col {
+  padding-left: 50px;
+  padding-right: 50px;
+  border: 1px solid rgba(76, 175, 79, 0);
+}
+.row {
+
+  padding-left: 50px;
+  padding-right: 50px;
+  border: 1px solid rgba(76, 175, 79, 0);
+}
+	input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+
+</style>
