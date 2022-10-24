1b91d7e6403919efaac40f0e48b9eeea543a0c6b
BarsanaXD <barsanaxd@gmail.com>
2022-10-24 13:52:44 +0800

    second

---
+++ src/views/Skills.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="container">
+<div class="row ">
+<div class="col">
+<div class="card" style="border-radius:50px;">
+<div class="card-header" style="border-radius:50px; background-color: rgb(82, 190, 252);" >
+<h1 style="text-align: center; padding: 10px;">Programming Skills</h1>
+
+</div>
+
+</div>
+</div>
+</div>
+<div class="row">
+<div class="card">
+<img src="" alt="" style="width:100%">
+<div class="container">
+<h4><b>HTML: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-red" style="height:24px;width:100%"></div>
+</div>
+</div>
+<div class="container">
+<h4><b>JAVA: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-blue" style="height:24px;width:100%"></div>
+</div>
+</div>  
+<div class="container">
+<h4><b>PYTHON: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-green" style="height:24px;width:80%"></div>
+</div>
+</div>
+<div class="container">
+<h4><b>JAVA SCRIPT: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-grey" style="height:24px;width:80%"></div>
+</div>
+</div>
+<div class="container">
+<h4><b>CSS: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-yellow" style="height:24px;width:80%"></div>
+</div>
+</div>
+<div class="container">
+<h4><b>DJANGO: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-red" style="height:24px;width:52%"></div>
+</div>
+</div>
+<div class="container">
+<h4><b>LARAVEL: </b></h4> 
+<div class="w3-light-grey">
+  <div class="w3-blue" style="height:24px;width:60%"></div>
+</div>
+</div>
+</div>
+<div class="w3-bar">
+</div>
+</div>
+</div>
+  </template>
